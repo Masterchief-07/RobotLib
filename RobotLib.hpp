6ab48c84ba5b15987effba0faf97ace4fed7922e
Masterchief-07 <kjonathan1999@hotmail.com>
2022-06-07 01:23:53 +0000

    base class

---
+++ RobotLib.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "Matrix/Matrix.hpp"
+#include <vector>
+#include <array>
+#include <iostream>
+
+class RobotLib
+{
+	//a lib to create and controls kinematics
+	public:
+		RobotLib();
+
+
+	private:
+		
+
+
+}
