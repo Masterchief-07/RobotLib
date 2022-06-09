5d7c8ce8c6506410383e5ffcecc557323e8da5f8
Masterchief-07 <kjonathan1999@hotmail.com>
2022-06-09 02:34:12 +0000

    improvement

---
+++ 2dofarm.py
@@ -0,0 +1,20 @@
+from RMatrix import RMatrix
+from sympy import pprint, symbols, Eq, Matrix, solveset,linsolve, nonlinsolve, solve
+
+theta_1, theta_2, l_1, l_2, x, y = symbols("theta_1 theta_2 l_1 l_2 x y");
+
+T_0_1 = RMatrix.D(theta_1, 0, l_1, 0);
+T_1_2 = RMatrix.D(theta_2, 0, l_2, 0);
+T_0_2 = T_0_1 * T_1_2
+equations = T_0_2[:2,3]
+a = Eq(equations[0],x)
+b = Eq(equations[1],y)
+pprint(T_0_1)
+pprint(T_1_2)
+pprint(T_0_2)
+pprint(T_0_2.rref())
+pprint(a)
+pprint(b)
+#pprint(solveset(Eq(a,x), theta_1))
+#pprint(linsolve([a,b], theta_1, theta_2))
+#pprint(nonlinsolve([a,b], theta_1, theta_2))
