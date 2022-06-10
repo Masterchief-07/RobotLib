4d0c84982a58bf4bb17cda73c75dd5e9727c3ab0
Masterchief-07 <kjonathan1999@hotmail.com>
2022-06-10 05:38:49 +0000

    in progress implementation of ccd in RobotLib

--- RobotLib.py
+++ RobotLib.py
@@ -1,6 +1,7 @@
 import sympy as sp
 from sympy import symbols, lambdify
 import numpy as np
+from numpy import ndarray
 from RMatrix import RMatrix
 from itertools import accumulate
 
@@ -10,6 +11,8 @@ class RobotLib:
         self.parts = [];
         self.parts_f = []; #parts function
         self.parts_p = []; #parts parameters
+        self.angles = [];
+        self.pos = [];
         self.T_0_N = None;
 
     def add(self, theta=None, alpha=None, r=None, d=None):
@@ -43,8 +46,55 @@ class RobotLib:
             forward kinematics of every T
         """
         assert(len(self.parts) == len(angles))
+        self.angles = angles;
         pos = [self.parts_f[i](angles[i]) for i in range(len(self.parts_f))]
         pos = tuple(accumulate(pos))
+        self.pos = pos;
         return pos;
+    
+    def ik(self, target:ndarray, algo:object):
+        """
+            inverse kinematics using a algo
+        """
+        algo.ik(self.angles, self.pos, self.parts_f, target); 
+        pass
+
+
+
+class CCD(object):
+    """
+        CCD algo class
+    """
+    def __init__(self, err_threshold = 0.001):
+        self.angles=[];
+        self.pos = [];
+        self.errors = 0;
+        self.err_threshold = err_threshold
+
+    def comput(self,v_t:ndarray, v_e:ndarray, v_i:ndarray):
+        """
+            comput ccd 
+        """
+        v_e_i = v_e - v_i;
+        v_t_i = v_t - v_i;
+
+        a = v_t_i / np.linalg.norm(v_t_i);
+        b = v_e_i / np.linalg.norm(v_i_i);
+
+        r = a.T.dot(b)[0].item();
+        angle = np.arccos(r);
+        return angle
+
+    def ik(self,angles:list, pos:list, funct:list, target:ndarray):
+        self.errors = 0;
+        self.angles = list(angles)
+        self.pos = list(pos)
+        while self.errors>self.err_threshold:
+            for i in reversed(range(len(funct)-1)):
+                pass
+                
+
+                
+
 
 
--- testRobotLib.py
+++ testRobotLib.py
@@ -1,5 +1,5 @@
 from RMatrix import RMatrix
-from RobotLib import RobotLib
+from RobotLib import RobotLib, CCD
 import sympy as sp
 import numpy as np
 
@@ -13,3 +13,5 @@ rb.add(alpha=0, r=2, d=0)
 
 pos = rb.fk([0.0,0.0,0.0,0.0,0.0])
 print(pos)
+algo = CCD();
+rb.ik(np.array([[2],[2],[0]]), algo);
